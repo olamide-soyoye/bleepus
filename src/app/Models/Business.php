330e097e435fb9355ddeb68d244232523a02d2fb
olamide-soyoye <you@example.com>
2023-11-14 10:07:32 +0100

    Register Feature fully implemented

--- src/app/Models/Business.php
+++ src/app/Models/Business.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Business extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'longitude',
+        'latitude',
+        'max_distance',
+        'ratings',
+        'company_name',
+    ];
 }
